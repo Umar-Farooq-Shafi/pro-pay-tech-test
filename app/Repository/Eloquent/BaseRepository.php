fe372f5674cd5e4824481f8ebfbbf10ad7345966
Umar-Farooq-Shafi <umarfarooqshafi2101@gmail.com>
2023-02-14 14:57:35 +0500

    BaseRepo

--- app/Repository/Eloquent/BaseRepository.php
+++ app/Repository/Eloquent/BaseRepository.php
@@ -38,16 +38,6 @@ class BaseRepository implements EloquentRepositoryInterface
     }
 
     /**
-     * Get all trashed models.
-     *
-     * @return Collection
-     */
-    public function allTrashed(): Collection
-    {
-        return $this->model->onlyTrashed()->get();
-    }
-
-    /**
      * Find model by id.
      *
      * @param int $modelId
@@ -63,28 +53,6 @@ class BaseRepository implements EloquentRepositoryInterface
         array $appends = []
     ): ?Model {
         return $this->model->select($columns)->with($relations)->findOrFail($modelId)->append($appends);
-    }
-
-    /**
-     * Find trashed model by id.
-     *
-     * @param int $modelId
-     * @return Model|null
-     */
-    public function findTrashedById(int $modelId): ?Model
-    {
-        return $this->model->withTrashed()->findOrFail($modelId);
-    }
-
-    /**
-     * Find only trashed model by id.
-     *
-     * @param int $modelId
-     * @return Model
-     */
-    public function findOnlyTrashedById(int $modelId): ?Model
-    {
-        return $this->model->onlyTrashed()->findOrFail($modelId);
     }
 
     /**
@@ -123,27 +91,5 @@ class BaseRepository implements EloquentRepositoryInterface
     public function deleteById(int $modelId): bool
     {
         return $this->findById($modelId)->delete();
-    }
-
-    /**
-     * Restore model by id.
-     *
-     * @param int $modelId
-     * @return bool
-     */
-    public function restoreById(int $modelId): bool
-    {
-        return $this->findOnlyTrashedById($modelId)->restore();
-    }
-
-    /**
-     * Permanently delete model by id.
-     *
-     * @param int $modelId
-     * @return bool
-     */
-    public function permanentlyDeleteById(int $modelId): bool
-    {
-        return $this->findTrashedById($modelId)->forceDelete();
     }
 }
